bb4affdcf96f315a8f0ae86a33deb6432142845a
AbdAyyad <abdayyad2012@gmail.com>
2019-04-29 11:57:50 +0300

    sql script for creating admins with password 12345678
    Signed-off-by: AbdAyyad <abdayyad2012@gmail.com>

---
+++ patches/create_admin.sql
@@ -0,0 +1,7 @@
+insert into user_table(user_name, user_email, user_password, user_id)
+values ('admin', 'admin@admin.com',
+        '$2a$12$Eu9HhZFnUfxq26MQW2AkLur1ElHI78qhqWXzo.lX1mlinsjHFhsJ2',
+        'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+insert into admin_table(admin_id, user_id)
+VALUES ('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
+        'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
