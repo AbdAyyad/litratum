c717cc87810b691f82567a7139608adb5f9fd451
AbdAyyad <abdayyad2012@gmail.com>
2019-05-02 11:15:44 +0300

    update data base
    Signed-off-by: AbdAyyad <abdayyad2012@gmail.com>

--- patches/data_base_16.sql
+++ patches/data_base_16.sql
@@ -1,3 +1,10 @@
 alter table user_table
     drop column logged_in;
 
+alter table unprocessed_content_table
+    alter column file_name type varchar(100) using file_name::varchar(100);
+
+alter table unprocessed_content_table
+    alter column time_stamp type varchar(100) using time_stamp::varchar(100);
+
+
