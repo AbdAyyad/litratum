5e707a5f5fc43ed9ff2d2dbe8c8e1410362091b2
AbdAyyad <abdayyad2012@gmail.com>
2019-05-03 10:26:08 +0300

    the column was added by mistake
    Signed-off-by: AbdAyyad <abdayyad2012@gmail.com>

--- patches/data_base_16.sql
+++ patches/data_base_16.sql
@@ -6,14 +6,3 @@ alter table unprocessed_content_table
 
 alter table unprocessed_content_table
     alter column time_stamp type varchar(100) using time_stamp::varchar(100);
-
-alter table user_table
-    add license_id varchar(64);
-
-create unique index user_table_license_id_uindex
-    on user_table (license_id);
-
-alter table user_table
-    add constraint user_table_license_table_license_id_fk
-        foreign key (license_id) references license_table (license_id)
-            on update cascade on delete cascade;
